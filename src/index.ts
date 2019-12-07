2f8b39e946a14a0880376118f66f720ad59c2c3f
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2019-12-07 11:19:34 +0100

    Export all from wsdl-types.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export type MetadataVersion = "47";
 export const latestMetadataVersion: MetadataVersion = "47";
 
 export * from "./wsdl/wsdl-parser";
+export * from "./wsdl/wsdl-types";
 export * from "./gen/typeScriptGenerator";
 export {
     readMetadataWSDLByVersion,
